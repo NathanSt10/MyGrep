a602c50ba41912572b8cc83d69f4736df25d34eb
NathanSt10 <nstout@uwm.edu>
2024-10-08 16:15:20 -0500

    File init

---
+++ grep.c
@@ -0,0 +1,10 @@
+#include "text.h"
+
+void grepLite(int argc, char *argv[]) {
+    // TODO: First check if the correct number of arguments is provided
+    
+
+
+    // TODO: Implement grep features
+
+}
